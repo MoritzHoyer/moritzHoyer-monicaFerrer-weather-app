9cdb2152edd8a7621ed4b6deec04d9f40105e5de
MONICA <monik.ferrer@gmail.com>
2024-09-03 10:57:35 +0200

    Added changes Fect and API

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,51 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Form from "./components/Form/Form.jsx";
+import List from "./components/List/List.jsx";
 import { uid } from "uid"; // Unique ID for each activity
 
 function App() {
-  // State for the list of activities
-  const [activities, setActivities] = useState([]);
+  // State for activities
+  const [activities, setActivities] = useState(() => {
+    // Load activities from the local storage, if available
+    const savedActivities = localStorage.getItem("activities");
+    return savedActivities ? JSON.parse(savedActivities) : [];
+  });
+
+  // State for weather - inicial state -
+
+  const [weather, setWeather] = useState({
+    temperature: null,
+    conditionEmoji: "❓",
+  });
+
+  // Fetch weather data on initial render . refactoring - API -
+
+  useEffect(() => {
+    fetch(
+      "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        const temperature = data.hourly.temperature_2m[0];
+        const conditionEmoji = getConditionEmoji(temperature);
+        setWeather({ temperature, conditionEmoji });
+      })
+      .catch((error) =>
+        console.error("Error fetching the weather data:", error)
+      );
+  }, []);
+
+  const getConditionEmoji = (temperature) => {
+    if (temperature >= 30) return "🌞";
+    if (temperature >= 20) return "🌤️";
+    if (temperature >= 10) return "🌥️";
+    return "🌧️";
+  };
+
+  // Saves activities in Local Storage when they change
+  useEffect(() => {
+    localStorage.setItem("activities", JSON.stringify(activities));
+  }, [activities]);
 
   // Function for adding a new activity
   function handleAddActivity(newActivity) {
@@ -17,9 +58,24 @@ function App() {
     setActivities([activityWithId, ...activities]);
   }
 
+  // Filter activities based on the weather
+  const isGoodWeather = weather ? weather.temperature > 15 : true; 
+  const filteredActivities = activities.filter(
+    (activity) => activity.isForGoodWeather === isGoodWeather
+  );
+
   return (
     <div>
-      <h1>Weather & Activities App </h1>
+      <List activities={filteredActivities} isGoodWeather={isGoodWeather} />
+      <h2>Weather & Activities App</h2>
+      {weather.temperature !== null ? (
+        <div>
+          <p>Temperature: {weather.temperature}°C</p>
+          <p>Condition: {weather.conditionEmoji}</p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
       {/* Pass the function for adding activities to the ActivityForm component */}
       <Form onAddActivity={handleAddActivity} />
     </div>
